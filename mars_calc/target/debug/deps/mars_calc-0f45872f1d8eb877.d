3946b2186bf4e99645ef2054606f7cb982f39031
DenisRomanovsky <denys.r@stadiumgoods.com>
2020-10-11 16:58:40 +0300

    gitignore

---
+++ mars_calc/target/debug/deps/mars_calc-0f45872f1d8eb877.d
@@ -0,0 +1,5 @@
+/home/dromanovsky/rust_study/mars_calc/target/debug/deps/mars_calc-0f45872f1d8eb877: src/main.rs
+
+/home/dromanovsky/rust_study/mars_calc/target/debug/deps/mars_calc-0f45872f1d8eb877.d: src/main.rs
+
+src/main.rs:
---
+++ mars_calc/target/debug/mars_calc.d
@@ -0,0 +1,1 @@
+/home/dromanovsky/rust_study/mars_calc/target/debug/mars_calc: /home/dromanovsky/rust_study/mars_calc/src/main.rs
