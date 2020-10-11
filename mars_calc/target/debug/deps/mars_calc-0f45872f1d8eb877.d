e1762d2ad23e69551b955fc2ea4108eb8e580d84
DenisRomanovsky <denys.r@stadiumgoods.com>
2020-10-11 17:01:45 +0300

    remove targets

--- mars_calc/target/debug/deps/mars_calc-0f45872f1d8eb877.d
+++
@@ -1,5 +0,0 @@
-/home/dromanovsky/rust_study/mars_calc/target/debug/deps/mars_calc-0f45872f1d8eb877: src/main.rs
-
-/home/dromanovsky/rust_study/mars_calc/target/debug/deps/mars_calc-0f45872f1d8eb877.d: src/main.rs
-
-src/main.rs:
--- mars_calc/target/debug/mars_calc.d
+++
@@ -1,1 +0,0 @@
-/home/dromanovsky/rust_study/mars_calc/target/debug/mars_calc: /home/dromanovsky/rust_study/mars_calc/src/main.rs
